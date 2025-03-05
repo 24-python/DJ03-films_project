f6700f5dc786b942959a8f14114caf1898053e08
24-python <24_python@mail.ru>
2025-03-05 18:47:10 +0300

    1

--- movie_project/films/views.py
+++ movie_project/films/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'films/index.html')
+
+def add_films(request):
+    return render(request, 'films/add_films.html')
+
